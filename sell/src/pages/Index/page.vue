4380229b5908c42dc4352aec91ff538c03640657
tongteng <19960406@icloud.com>
2018-05-10 18:04:34 +0800

    内容区与侧边栏

--- sell/src/pages/Index/page.vue
+++ sell/src/pages/Index/page.vue
@@ -5,6 +5,112 @@
 	<div>
 		<navheader></navheader>
 		<introheader></introheader>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-1 col-md-8 col-md-offset-1 col-sm-12 col-xs-12 postlist-container">
+					<div class="post-preview">
+						<a href="">
+							<h2>ES6(四) -- 函数的扩展</h2>
+							<h3>rest参数、严格模式、name属性、箭头函数</h3>
+							<div class="post-content-preview">
+								 yi蓑烟雨任平生2、rest参数顾名思义，rest参数字面意思大概就是剩下的参数的意思，用于获取函数参数的剩下的参数，rest参数为一个数组，类似于arguments对象，这个变量将参数中剩下的变量都放入到数组中，而且这个数组就是一个真正的数组，而不是像arguments那样是一个类数组，并不是一个真正的数组。function add (...values) {let...
+							</div>
+						</a>
+						<p class="post-meta">Posted by HandSomeTT on February 11, 2018</p>						
+					</div>
+					<hr>
+					<div class="post-preview">
+						<a href="">
+							<h2>ES6(四) -- 函数的扩展</h2>
+							<h3>rest参数、严格模式、name属性、箭头函数</h3>
+							<div class="post-content-preview">
+								 yi蓑烟雨任平生2、rest参数顾名思义，rest参数字面意思大概就是剩下的参数的意思，用于获取函数参数的剩下的参数，rest参数为一个数组，类似于arguments对象，这个变量将参数中剩下的变量都放入到数组中，而且这个数组就是一个真正的数组，而不是像arguments那样是一个类数组，并不是一个真正的数组。function add (...values) {let...
+							</div>
+						</a>
+						<p class="post-meta">Posted by HandSomeTT on February 11, 2018</p>						
+					</div>
+					<hr>
+					<div class="post-preview">
+						<a href="">
+							<h2>ES6(四) -- 函数的扩展</h2>
+							<h3>rest参数、严格模式、name属性、箭头函数</h3>
+							<div class="post-content-preview">
+								 yi蓑烟雨任平生2、rest参数顾名思义，rest参数字面意思大概就是剩下的参数的意思，用于获取函数参数的剩下的参数，rest参数为一个数组，类似于arguments对象，这个变量将参数中剩下的变量都放入到数组中，而且这个数组就是一个真正的数组，而不是像arguments那样是一个类数组，并不是一个真正的数组。function add (...values) {let...
+							</div>
+						</a>
+						<p class="post-meta">Posted by HandSomeTT on February 11, 2018</p>						
+					</div>
+					<hr>
+					<div class="post-preview">
+						<a href="">
+							<h2>ES6(四) -- 函数的扩展</h2>
+							<h3>rest参数、严格模式、name属性、箭头函数</h3>
+							<div class="post-content-preview">
+								 yi蓑烟雨任平生2、rest参数顾名思义，rest参数字面意思大概就是剩下的参数的意思，用于获取函数参数的剩下的参数，rest参数为一个数组，类似于arguments对象，这个变量将参数中剩下的变量都放入到数组中，而且这个数组就是一个真正的数组，而不是像arguments那样是一个类数组，并不是一个真正的数组。function add (...values) {let...
+							</div>
+						</a>
+						<p class="post-meta">Posted by HandSomeTT on February 11, 2018</p>						
+					</div>
+					<hr>
+					<div class="post-preview">
+						<a href="">
+							<h2>ES6(四) -- 函数的扩展</h2>
+							<h3>rest参数、严格模式、name属性、箭头函数</h3>
+							<div class="post-content-preview">
+								 yi蓑烟雨任平生2、rest参数顾名思义，rest参数字面意思大概就是剩下的参数的意思，用于获取函数参数的剩下的参数，rest参数为一个数组，类似于arguments对象，这个变量将参数中剩下的变量都放入到数组中，而且这个数组就是一个真正的数组，而不是像arguments那样是一个类数组，并不是一个真正的数组。function add (...values) {let...
+							</div>
+						</a>
+						<p class="post-meta">Posted by HandSomeTT on February 11, 2018</p>						
+					</div>
+					<hr>
+					<div class="post-preview">
+						<a href="">
+							<h2>ES6(四) -- 函数的扩展</h2>
+							<h3>rest参数、严格模式、name属性、箭头函数</h3>
+							<div class="post-content-preview">
+								 yi蓑烟雨任平生2、rest参数顾名思义，rest参数字面意思大概就是剩下的参数的意思，用于获取函数参数的剩下的参数，rest参数为一个数组，类似于arguments对象，这个变量将参数中剩下的变量都放入到数组中，而且这个数组就是一个真正的数组，而不是像arguments那样是一个类数组，并不是一个真正的数组。function add (...values) {let...
+							</div>
+						</a>
+						<p class="post-meta">Posted by HandSomeTT on February 11, 2018</p>						
+					</div>
+					<hr>
+				</div>
+				<div class="col-lg-3 col-lg-offset-0 col-md-3 col-md-offset-0 col-sm-12 col-xs-12 sidebar-container">
+					<section>
+						<hr class="hidden-sm hidden-xs">
+						<h5><a href="">FEATURED TAGS</a></h5>
+						<div class="tags">
+							<a href="">记录生活</a>
+							<a href="">面试总结</a>
+							<a href="">HTML</a>
+							<a href="">CSS</a>
+							<a href="">Javascript</a>
+						</div>
+					</section>
+					<section class="visible-md visible-lg">
+						<hr>
+						<h5><a href="">ABOUT ME</a></h5>
+						<div class="short-about">
+							<img src="" alt="">
+							<p>一蓑烟雨任平生</p>
+							<ul class="list-inline">
+								<li><a href=""><span class="fa-stack fa-lg"><i></i><i></i></span></a></li>
+								<li><a href=""><span class="fa-stack fa-lg"><i></i><i></i></span></a></li>
+								<li><a href=""><span class="fa-stack fa-lg"><i></i><i></i></span></a></li>
+								<li><a href=""><span class="fa-stack fa-lg"><i></i><i></i></span></a></li>
+							</ul>
+						</div>
+					</section>
+					<hr>
+					<h5>LIKES</h5>
+					<ul class="list-inline">
+						<li><a href="">阮一峰</a></li>
+						<li><a href="">张冰洁</a></li>
+						<li><a href="">刘迪康</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 <script src="./page.js">
